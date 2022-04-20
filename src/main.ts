19654aad22ed9d950b68a18fadb98fa2c022ceba
蒋军晖 <mobeigege@icloud.com>
2022-04-20 15:04:57 +0800

    内容更改

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import { Lazyload } from 'vant'
-import App from './Main.vue'
-import router from './router/index'
-const app = createApp(App)
+import { createApp } from 'vue';
+import { Lazyload } from 'vant';
+import App from '@/Main.vue';
+import router from '@/router/index';
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 app.use(Lazyload, {
 	lazyComponent: true
-})
-app.mount('#app')
+});
+app.mount('#app');
